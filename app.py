8bf700cfc34354718d171d2f1498f282a51d417c
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-29 12:00:37 +0800

    file added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-def getAllBookstore() ->list:
+def getAllBookstore() -> list:
     url = "https://cloud.culture.tw/frontsite/trans/emapOpenDataAction.do?method=exportEmapJson&typeId=M"
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers)
@@ -45,32 +45,34 @@ def getBookstoreInfo(items):
         expander.image(item['representImage'])
         expander.metric('hitRate', item['hitRate'])
         expander.subheader('Introduction')
-        # 用 st.write 呈現書店的 Introduction
+        expander.write(item['intro'])
         expander.subheader('Address')
-        # 用 st.write 呈現書店的 Address
+        expander.write(item['address'])
         expander.subheader('Open Time')
-        # 用 st.write 呈現書店的 Open Time
+        expander.write(item['openTime'])
         expander.subheader('Email')
-	      # 用 st.write 呈現書店的 Email
-        # 將該 expander 放到 expanderList 中
+        expander.write(item['email'])
+        expanderList.append(expander)
     return expanderList
 
 def app():
-	bookstoreList = getAllBookstore()
+    bookstoreList = getAllBookstore()
 
-	countyOption = getCountyOption(bookstoreList)
+    countyOption = getCountyOption(bookstoreList)
 
-  st.header('特色書店地圖')
-	st.metric('Total bookstore', len(bookstoreList))
-	county = st.selectbox('請選擇縣市', countyOption) 
-	districtOption = getDistrictOption(bookstoreList, county)
-	district = st.multiselect('請選擇區域', districtOption) 
-	
-	specificBookstore = getSpecificBookstore(bookstoreList, county, district)
-	num = len(specificBookstore)
-	st.write(f'總共有{num}項結果', num)
-	
-	# 呼叫 getBookstoreInfo 並將回傳值賦值給變數 bookstoreInfo
+    st.header('特色書店地圖')
+    st.metric('Total bookstore', len(bookstoreList))
+    county = st.selectbox('請選擇縣市', countyOption)
+    districtOption = getDistrictOption(bookstoreList, county)
+    district = st.multiselect('請選擇區域', districtOption)
+
+    specificBookstore = getSpecificBookstore(bookstoreList, county, district)
+    num = len(specificBookstore)
+    st.write(f'總共有{num}項結果', num)
+		
+	# 幫 specificBookstore 進行排序
+	# hint: 使用 lambda 和 sort
+    bookstoreInfo = getBookstoreInfo(specificBookstore)
 
 if __name__ == '__main__':
     app()
