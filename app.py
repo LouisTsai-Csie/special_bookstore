21cd0cd5198c14ab7b0ad50a77011ea7b9b27f49
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-29 11:54:51 +0800

    space changed

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def getCountyOption(items) ->list:
     # 創建一個空的 List 並命名為 optionList
     for item in items:
         # 把 cityname 欄位中的縣市名稱擷取出來 並指定給變數 name
-				# hint: 想辦法處理 item['cityName'] 的內容
+		# hint: 想辦法處理 item['cityName'] 的內容
 
         # 如果 name 不在 optionList 之中，便把它放入 optionList
-				# hint: 使用 if-else 來進行判斷 / 用 append 把東西放入 optionList
+		# hint: 使用 if-else 來進行判斷 / 用 append 把東西放入 optionList
     return optionList
 
 def app():
