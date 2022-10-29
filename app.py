dd5d282b05c56838a73073b3e59dd708cbe7802a
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-29 11:52:30 +0800

    file changed

--- app.py
+++ app.py
@@ -1,77 +1,19 @@
 import streamlit as st
 import requests
 
-def getAllBookstore() -> list:
-    url = "https://cloud.culture.tw/frontsite/trans/emapOpenDataAction.do?method=exportEmapJson&typeId=M"
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
-    res = response.json()
-    return res
-
-def getCountyOption(items) ->list:
-    optionList = []
-    for item in items:
-        name = item['cityName'][0:3]
-        if name not in optionList:
-            optionList.append(name)
-    return optionList
-
-def getDistrictOption(items, target) ->list:
-    optionList = []
-    for item in items:
-        name = item['cityName']
-        if target not in name: continue
-        name.strip()
-        district = name[5:]
-        if len(district) == 0: continue
-        if district not in optionList:
-            optionList.append(district)
-    return optionList
-
-def getSpecificBookstore(items, county, districts):
-    specificBookstoreList = []
-    for item in items:
-        name = item['cityName']
-        if county not in name: continue
-        for district in districts:
-            if district not in name: continue
-            specificBookstoreList.append(item)
-    return specificBookstoreList
-
-def getBookstoreInfo(items):
-    expanderList = []
-    for item in items:
-        expander = st.expander(item['name'])
-        expander.image(item['representImage'])
-        expander.metric('hitRate', item['hitRate'])
-        expander.subheader('Introduction')
-        expander.write(item['intro'])
-        expander.subheader('Address')
-        expander.write(item['address'])
-        expander.subheader('Open Time')
-        expander.write(item['openTime'])
-        expander.subheader('Email')
-        expander.write(item['email'])
-        expanderList.append(expander)
-    return expanderList
+def getAllBookstore() ->list:
+	url = '' # 在這裡輸入目標 url
+	headers = {"accept": "application/json"}
+	response = request.get(url, headers=headers)
+	# 將 response 轉換成 json 格式
+	# 回傳值
 
 def app():
-    bookstoreList = getAllBookstore()
-
-    countyOption = getCountyOption(bookstoreList)
-
+	# 呼叫 getAllBookstore 函式並將其賦值給變數 bookstoreList
     st.header('特色書店地圖')
-    st.metric('Total bookstore', len(bookstoreList))
-    county = st.selectbox('請選擇縣市', countyOption)
-    districtOption = getDistrictOption(bookstoreList, county)
-    district = st.multiselect('請選擇區域', districtOption)
-
-    specificBookstore = getSpecificBookstore(bookstoreList, county, district)
-    num = len(specificBookstore)
-    st.write(f'總共有{num}項結果', num)
-
-    specificBookstore.sort(key = lambda item: item['hitRate'], reverse=True)
-    bookstoreInfo = getBookstoreInfo(specificBookstore)
+	st.metric('Total bookstore', 118) # 將 118 替換成書店的數量
+	county = st.selectbox('請選擇縣市', ['A', 'B', 'C'])
+	district = st.multiselect('請選擇區域', ['a', 'b', 'c', 'd'])
 
 if __name__ == '__main__':
     app()
