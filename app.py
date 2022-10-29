1d11c709d355332654004856665ad80fb60b1182
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-29 11:59:30 +0800

    file added

--- app.py
+++ app.py
@@ -32,29 +32,45 @@ def getSpecificBookstore(items, county, districts):
     specificBookstoreList = []
     for item in items:
         name = item['cityName']
-        # 如果 name 不是我們選取的 county 則跳過
-		# hint: 用 if-else 判斷並用 continue 跳過
-        
-		# districts 是一個 list 結構，判斷 list 每個值是否出現在 name 之中
-		# 判斷該項目是否已經出現在 specificBookstoreList 之中，沒有則放入
-		# hint: 用 for-loop 進行迭代，用 if-else 判斷，用 append 放入
+        if county not in name: continue
+        for district in districts:
+            if district not in name: continue
+            specificBookstoreList.append(item)
     return specificBookstoreList
+
+def getBookstoreInfo(items):
+    expanderList = []
+    for item in items:
+        expander = st.expander(item['name'])
+        expander.image(item['representImage'])
+        expander.metric('hitRate', item['hitRate'])
+        expander.subheader('Introduction')
+        # 用 st.write 呈現書店的 Introduction
+        expander.subheader('Address')
+        # 用 st.write 呈現書店的 Address
+        expander.subheader('Open Time')
+        # 用 st.write 呈現書店的 Open Time
+        expander.subheader('Email')
+	      # 用 st.write 呈現書店的 Email
+        # 將該 expander 放到 expanderList 中
+    return expanderList
 
 def app():
 	bookstoreList = getAllBookstore()
 
 	countyOption = getCountyOption(bookstoreList)
-    
-    st.header('特色書店地圖')
+
+  st.header('特色書店地圖')
 	st.metric('Total bookstore', len(bookstoreList))
 	county = st.selectbox('請選擇縣市', countyOption) 
 	districtOption = getDistrictOption(bookstoreList, county)
 	district = st.multiselect('請選擇區域', districtOption) 
 	
-	# 呼叫 getSpecificBookstore 並將回傳值賦值給變數 specificBookstore
+	specificBookstore = getSpecificBookstore(bookstoreList, county, district)
 	num = len(specificBookstore)
-	# 用 st.write 將目標書店的總數量計算出來，格式：總共有3項結果
+	st.write(f'總共有{num}項結果', num)
 	
+	# 呼叫 getBookstoreInfo 並將回傳值賦值給變數 bookstoreInfo
 
 if __name__ == '__main__':
     app()
