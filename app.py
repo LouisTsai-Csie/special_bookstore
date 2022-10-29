36dfbb891a257ef5d63e9a04bca9be57e4191cdd
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-29 11:53:50 +0800

    file changed

--- app.py
+++ app.py
@@ -2,17 +2,30 @@ import streamlit as st
 import requests
 
 def getAllBookstore() ->list:
-	url = '' # 在這裡輸入目標 url
-	headers = {"accept": "application/json"}
-	response = request.get(url, headers=headers)
-	# 將 response 轉換成 json 格式
-	# 回傳值
+    url = "https://cloud.culture.tw/frontsite/trans/emapOpenDataAction.do?method=exportEmapJson&typeId=M"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    res = response.json()
+    return res
+
+def getCountyOption(items) ->list:
+    # 創建一個空的 List 並命名為 optionList
+    for item in items:
+        # 把 cityname 欄位中的縣市名稱擷取出來 並指定給變數 name
+				# hint: 想辦法處理 item['cityName'] 的內容
+
+        # 如果 name 不在 optionList 之中，便把它放入 optionList
+				# hint: 使用 if-else 來進行判斷 / 用 append 把東西放入 optionList
+    return optionList
 
 def app():
-	# 呼叫 getAllBookstore 函式並將其賦值給變數 bookstoreList
+	bookstoreList = getAllBookstore()
+
+	# 呼叫 getCountyOption 並將回傳值賦值給變數 countyOption
+
     st.header('特色書店地圖')
-	st.metric('Total bookstore', 118) # 將 118 替換成書店的數量
-	county = st.selectbox('請選擇縣市', ['A', 'B', 'C'])
+	st.metric('Total bookstore', len(bookstoreList))
+	county = st.selectbox('請選擇縣市', ['A', 'B', 'C']) # 將['A', 'B', 'C']替換成縣市選項 
 	district = st.multiselect('請選擇區域', ['a', 'b', 'c', 'd'])
 
 if __name__ == '__main__':
